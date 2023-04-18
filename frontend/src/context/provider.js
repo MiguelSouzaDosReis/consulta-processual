524bb0c39441c859afae5ee498dcdca18d7872d5
Miguel Reis <guelsouzar@gmail.com>
2023-04-18 07:47:52 -0300

    feat: improving the option

--- frontend/src/context/provider.js
+++ frontend/src/context/provider.js
@@ -1,30 +1,26 @@
-
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import MyContext from './myContext.js';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import MyContext from "./myContext.js";
 
 function Provider({ children }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [document, setDocument] = useState([]);
+  const [lawCourt, setLawCourt] = useState([])
 
-    const [searchTerm, setSearchTerm] = useState("")
-    const [document, setDocument] = useState([]);
+  const context = {
+    searchTerm,
+    setSearchTerm,
+    document,
+    setDocument,
+    lawCourt,
+    setLawCourt
+  };
 
-
-    const context = {
-        searchTerm,
-        setSearchTerm,
-        document,
-        setDocument
-    };
-
-    return (
-        <MyContext.Provider value={ context }>
-            { children }
-        </MyContext.Provider>
-    );
+  return <MyContext.Provider value={context}>{children}</MyContext.Provider>;
 }
 
 Provider.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default Provider;
--- frontend/src/pages/search.jsx
+++ frontend/src/pages/search.jsx
@@ -1,51 +1,82 @@
-import React, { useContext } from 'react';
-import MyContext from '../context/myContext';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom'
-
+import React, { useContext } from "react";
+import MyContext from "../context/myContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function Search() {
-  const {  searchTerm, setSearchTerm, setDocument } = useContext(MyContext);
+  const { searchTerm, setSearchTerm, setDocument, lawCourt, setLawCourt } =
+    useContext(MyContext);
 
-  let navigate = useNavigate()
-
+  let navigate = useNavigate();
 
   const handleClick = async () => {
-    navigate('/search')
-    await axios.get(`http://localhost:3005/search/${searchTerm}`).then((response) => {
-      if (response.data.length <= 4){
-        setDocument([... response.data])
-      } else {
-        setDocument([response.data])
-      }
-    })
-  }
+    navigate("/search");
+    await axios
+      .get(`http://localhost:3005/search/${searchTerm}`)
+      .then((response) => {
+        if (response.data.length <= 4) {
+          setDocument([...response.data]);
+        } else {
+          setDocument([response.data]);
+        }
+      });
+  };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3005/lawsuit")
+      .then((response) => {
+        setLawCourt(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const tribunaisUnicos = Array.from(new Set(lawCourt.map((element) => element.tribunal)));
 
   return (
-    <div className='box'>
-      <h3 className='text'>Busca</h3>
-      <p className='text'>Selecione um tribunal para lista os processos ou buscar pelo número unificado</p>
-      <div className='searchItens'>
-
+    <div className="box">
+      <h3 className="text">Busca</h3>
+      <p className="text">
+        Selecione um tribunal para lista os processos ou buscar pelo número
+        unificado
+      </p>
+      <div className="searchItens">
         <select
-          className='option'
+          className="option"
           id="Tribunal"
           value={searchTerm}
-          onChange={(event) => setSearchTerm(event.target.value) }>
-        <option value="" selected> Número de Processo</option>
-          <option value="TJMG">TJMG</option>
+          onChange={(event) => setSearchTerm(event.target.value)}
+        >
+          <option value="" selected>
+            {" "}
+            Número de Processo
+          </option>
+          {
+
+
+            tribunaisUnicos.map((tribunal) => (
+              <option key={tribunal}>{tribunal}</option>
+            ))
+
+          }
+          {/* <option value="TJMG">TJMG</option>
           <option value="TJRS">TJRS</option>
-          <option value="TJSP">TJSP</option>
+          <option value="TJSP">TJSP</option> */}
         </select>
 
         <input
           type="text"
-          value={ searchTerm }
-          onChange={(event) => setSearchTerm(event.target.value) }
-          className='input'
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          className="input"
           id="cnj"
         />
-        <button className='button' onClick={ handleClick }>Buscar</button>
+        <button className="button" onClick={handleClick}>
+          Buscar
+        </button>
       </div>
     </div>
   );
