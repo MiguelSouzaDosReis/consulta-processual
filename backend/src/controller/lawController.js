7878e222ef8e0fb3e9faf7ca5f333360949c617b
Miguel Reis <guelsouzar@gmail.com>
2023-04-18 07:47:32 -0300

    feat: add readAll

--- backend/src/controller/lawController.js
+++ backend/src/controller/lawController.js
@@ -1,15 +1,21 @@
-const LawModel = require('../model/lawModel')
+const LawModel = require("../model/lawModel");
 
 module.exports = {
-    async readOne(req, res){
-        const { lawsuit } = req.params
-        if(lawsuit.length <= 4){
-            const tri = await LawModel.find({"tribunal": lawsuit});
-            console.log(tri)
-            return res.json(tri);
-        }
-        const cnj = await LawModel.findOne({"cnj": lawsuit});
-                console.log(cnj)
-                return res.json(cnj);
-    },
-}
+  async readOne(req, res) {
+    const { lawsuit } = req.params;
+    if (lawsuit.length <= 4) {
+      const tri = await LawModel.find({ tribunal: lawsuit });
+      console.log(tri);
+      return res.json(tri);
+    }
+    const cnj = await LawModel.findOne({ cnj: lawsuit });
+    console.log(cnj);
+    return res.json(cnj);
+  },
+
+  async readAll(req_, res) {
+      const readEverything = await LawModel.find();
+      console.log(readEverything);
+      return res.json(readEverything);
+  },
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const routes = express.Router()
-const lawController = require('./controller/lawController')
+const express = require("express");
+const routes = express.Router();
+const lawController = require("./controller/lawController");
 
-routes.get('/search/:lawsuit', lawController.readOne)
+routes.get("/search/:lawsuit", lawController.readOne);
+routes.get("/lawsuit", lawController.readAll);
 
-module.exports = routes
+module.exports = routes;
